fad5ff5e00f52c6537f3432df008f783dea70516
Peter White <pwhitedeveloper@gmail.com>
2019-05-25 15:45:15 -0400

    cart db added

--- app/models.py
+++ app/models.py
@@ -6,7 +6,13 @@ class Game(db.Model):
     descriptionURL = db.Column(db.String(50), nullable=False)
     price = db.Column(db.Float, nullable=False)
     imageURL = db.Column(db.String(50), nullable=False)
-    rating = db.Column(db.String(2))
+    rating = db.Column(db.String(2), nullable=False)
     type = db.Column(db.String(20), nullable=False)
     genre = db.Column(db.String(50), nullable=False)
+    quantity = db.Column(db.Integer)
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
     quantity = db.Column(db.Integer)
